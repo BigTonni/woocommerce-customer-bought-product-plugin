e0844583c7b0875bae5cc79bde8ee9408000941a
Gabor Javorszky <gabor@javorszky.co.uk>
2017-09-10 18:02:55 +0100

    Initial commit

---
+++ wc-customer-bought-product-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WooCommerce Customer Bought Product Experimental Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Gabor Javorszky <gabor@javorszky.co.uk>
+ * Author URI:      https://javorszky.co.uk
+ * Text Domain:     wc-customer-bought-product-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wc_Customer_Bought_Product_Plugin
+ */
